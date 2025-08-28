7e14db62b01380d70133f7219c0040218622cf2b
PhamThiTraMy <tramy06spt@gmail.com>
2025-08-28 22:45:17 +0700

    init project

---
+++ Project28_8/src/main/java/com/mycompany/project28_8/Project28_8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project28_8;
+
+/**
+ *
+ * @author TraMy
+ */
+public class Project28_8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
